72287f2afdc2cded3daa3de598a86caf319539d4
Kevin Burke <kev@inburke.com>
2018-04-28 13:42:02 -0700

    0.4

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ Annotate stdin with timestamps per line.
 	}
 }
 
-const Version = "0.3"
+const Version = "0.4"
 
 func main() {
 	version := flag.Bool("version", false, "Print the version string")
